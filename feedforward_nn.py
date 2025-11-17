c3995283c0734396fd0892680a5a11cb581ca8eb
Daniel Mamaghani <daniel.mamaghani@gmail.com>
2025-11-16 19:34:35 -0800

    instead of slower, sigmoid transfer function, use ReLU (replace negative vals with 0).

--- feedforward_nn.py
+++ feedforward_nn.py
@@ -20,15 +20,18 @@ class NeuralNet(object):
                 j += 1
     
     def evaluate(self, x: np.ndarray):
-        print(f"{x=}")
-
         curarray = x
         for i in range(len(self.topology)-1): 
             curarray = np.dot(curarray, self.weight_mats[i])
+            curarray = self.relu(curarray) # transfer function
             pprint(f"{curarray=}")
-        
-        print("output:")
-        pprint(curarray)
+
+    # replace all negative array values with 0. 
+    def relu(self, x: np.ndarray):
+        for i, val in enumerate(x):
+            if val < 0:
+                x[i] = 0
+        return x
         
 
 a = NeuralNet([3, 4, 2])
@@ -37,4 +40,4 @@ pprint(f"{a.topology=}")
 pprint(f"{a.weight_mats=}")
 
 print("evaluate:")
-a.evaluate([1, 2, 3])
+a.evaluate([-1, 2, -3])
